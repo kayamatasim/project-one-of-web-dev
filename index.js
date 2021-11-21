576a2c18a177ec14dfdb84f35220b18b07374353
asimsheikh@lidolearning.com <asmsheikh123@gmail.com>
2021-11-21 14:06:42 +0530

    function prototype and bind method

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
-console.log('first project')
+function Person(name,yob,designation) {
+    this.name=name;
+    this.yob=yob;
+    this.designation=designation;
+}
+Person.prototype.getage=function () 
+{
+    console.log(`the current age is ${2021-this.yob}`)    
+}
+let asim=new Person('asim',1997,'tutor');
+asim.getage()
+
+function praise(age) {
+console.log(`${this.name} is ${age} year old`)
+    
+}
+let kasim={
+    name:'kasim'
+}
+praise.call(kasim,22)
